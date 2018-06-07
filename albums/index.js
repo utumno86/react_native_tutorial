267ff6fe7088b2057be69a067751fe0afc9b31f3
Michael Nash <michael@spatialnetworks.com>
2018-06-07 17:01:49 -0400

    some small progress

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,7 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+
+const App = () => <Text>Some Text</Text>;
 
 AppRegistry.registerComponent('albums', () => App);
