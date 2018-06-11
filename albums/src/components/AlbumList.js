247a8cd280273ac698b98c94a1a545a65d73d23b
Michael Nash <michael@spatialnetworks.com>
2018-06-11 17:32:07 -0400

    styled card component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
-  componentWillMount() {
+  state = { albums: [] };
 
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
     return (
       <View>
-        <Text> Album List!!!! </Text>
+        {this.renderAlbums()}
       </View>
     )
   }
