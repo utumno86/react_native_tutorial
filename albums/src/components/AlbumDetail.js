572cb868f77e94f65d4039cd207613e8b537a21d
Michael Nash <michael@spatialnetworks.com>
2018-06-18 18:49:32 -0400

    added a button

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -19,6 +20,9 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
       <CardSection>
         <Image style={ imageStyle } source={{ uri: image}} />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
