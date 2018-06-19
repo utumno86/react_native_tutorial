40741b09744d4be69bf7e0cba481b49932a768ca
Michael Nash <michael@spatialnetworks.com>
2018-06-19 14:06:40 -0400

    input component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import { Card, CardSection, Button } from './common'
+import { Card, CardSection, Button, Input } from './common'
 
 class LoginForm extends Component {
+  state = { email: '' }
   render () {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            onChangeText={email => this.setState({ email })}
+            value={this.state.email}
+          />
+        </CardSection>
+        <CardSection>
+        </CardSection>
         <CardSection>
           <Button>
             Log in
