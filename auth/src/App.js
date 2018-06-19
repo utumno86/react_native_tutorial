bf73de36c5928c2d4c85d570bfb98dcf0764a574
Michael Nash <michael@spatialnetworks.com>
2018-06-19 13:03:51 -0400

    initialized login form

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import { Header } from './components/common'
+import firebase from 'firebase'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  componentWillMount () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDdWbkue9zJSy_qfyhd7C28W2LlkH56sAs',
+      authDomain: 'authentication-34b74.firebaseapp.com',
+      databaseURL: 'https://authentication-34b74.firebaseio.com',
+      projectId: 'authentication-34b74',
+      storageBucket: 'authentication-34b74.appspot.com',
+      messagingSenderId: '674295334392'
+    })
+  }
+
   render () {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>
-          An App
-        </Text>
+        <LoginForm />
       </View>
     )
   }
