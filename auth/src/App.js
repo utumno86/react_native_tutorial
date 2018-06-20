51d6a3d85499ad5a05bf227d72218d75348e8652
Michael Nash <michael@spatialnetworks.com>
2018-06-20 14:07:16 -0400

    completed authentication flow

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { Header } from './components/common'
+import { Header, Button, Spinner } from './components/common'
 import firebase from 'firebase'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount () {
     firebase.initializeApp({
       apiKey: 'AIzaSyDdWbkue9zJSy_qfyhd7C28W2LlkH56sAs',
@@ -14,13 +16,36 @@ class App extends Component {
       storageBucket: 'authentication-34b74.appspot.com',
       messagingSenderId: '674295334392'
     })
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ loggedIn: true })
+    } else {
+      this.setState({ loggedIn: false })
+    }
+  })
+}
+
+renderContent() {
+  switch (this.state.loggedIn) {
+    case true:
+      return (
+        <Button onPress={() => firebase.auth().signOut()}>
+          Log Out
+        </Button>
+      )
+    case false:
+      return <LoginForm />
+    default:
+      return <Spinner size="large" />
   }
+}
 
   render () {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        { this.renderContent() }
       </View>
     )
   }
