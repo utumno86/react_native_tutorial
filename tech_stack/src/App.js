988c6b2c822b8c273167fab9f651feb70b29f202
Michael Nash <michael@spatialnetworks.com>
2018-06-21 12:33:20 -0400

    wired up redux and copied over common components

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { View } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+import { Header } from './components/common'
 
 const App = () => {
   return (
-    <View />
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
+    </Provider>
   )
 }
 
