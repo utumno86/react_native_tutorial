6dc2eaf1e9baa275b18d22005896154f145f0add
Michael Nash <michael@spatialnetworks.com>
2018-06-25 12:19:12 -0400

    slight refactor

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -6,9 +6,8 @@ import * as actions from '../actions'
 
 class ListItem extends Component {
   renderDescription () {
-    const { library, selectedLibraryId } = this.props
-    console.log(this.props)
-    if (library.id === selectedLibraryId) {
+    const { library, expanded } = this.props
+    if (expanded) {
       return (
         <Text>
           {library.description}
@@ -44,8 +43,10 @@ const styles = {
   }
 }
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId }
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id
+
+  return { expanded }
 }
 
 export default connect(mapStateToProps, actions)(ListItem)
