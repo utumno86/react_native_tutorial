136b99202e9e247ca79f66f7acf1042aafe79eb4
Michael Nash <michael@spatialnetworks.com>
2018-06-26 12:06:41 -0400

    initial setup

---
+++ manager/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import firebase from 'firebase'
+import reducers from './reducers'
+
+class App extends Component {
+  componentWillMount () {
+    const config = {
+      apiKey: 'AIzaSyDuR6h57agPOPUXdqzquhg77kMGl4cWlP0',
+      authDomain: 'manager-d89fd.firebaseapp.com',
+      databaseURL: 'https://manager-d89fd.firebaseio.com',
+      projectId: 'manager-d89fd',
+      storageBucket: 'manager-d89fd.appspot.com',
+      messagingSenderId: '477439549563'
+    }
+
+    firebase.initializeApp(config)
+  }
+
+  render () {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App
