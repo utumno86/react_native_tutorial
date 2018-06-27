53c78a53e6c7904519b09ad76a6321b683b65b5c
Michael Nash <michael@spatialnetworks.com>
2018-06-27 16:11:59 -0400

    initialized the login user action

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import firebase from 'firebase'
+import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
 import LoginForm from './components/LoginForm'
 
-const store = createStore(reducers)
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
 
 class App extends Component {
   componentWillMount () {
