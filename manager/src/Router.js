93bd5aca636721e9739a0582ea432374d0d6a9c9
Michael Nash <michael@spatialnetworks.com>
2018-06-28 18:42:24 -0400

    wired up a new action file and corresponding reducers

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -17,7 +17,9 @@ const RouterComponent = () => {
             onRight={() => Actions.employeeCreate() }
             key="employeeList"
             component={EmployeeList}
-            title="Employees"/>
+            title="Employees"
+            initial
+          />
           <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee">
           </Scene>
         </Scene>
--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { Card, CardSection, Input, Button, Spinner } from './common'
 import { connect } from 'react-redux'
 
 class EmployeeCreate extends Component {
   render () {
     return (
-      <View>
-        <Text> Employee Form </Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     )
   }
 }
