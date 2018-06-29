4c3954f77730c78e0335551c98ea8ff3589d935d
Michael Nash <michael@spatialnetworks.com>
2018-06-29 12:08:46 -0400

    wired up employee create page to reducers

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Card, CardSection, Input, Button, Spinner } from './common'
 import { connect } from 'react-redux'
+import { employeeUpdate } from '../actions'
+import { Card, CardSection, Input, Button, Spinner } from './common'
 
 class EmployeeCreate extends Component {
   render () {
@@ -10,6 +11,8 @@ class EmployeeCreate extends Component {
           <Input
             label="Name"
             placeholder="Jane"
+            value={this.props.name}
+            onChangeText={ text => this.props.employeeUpdate({ prop: 'name', value: text })}
           />
         </CardSection>
 
@@ -17,6 +20,8 @@ class EmployeeCreate extends Component {
           <Input
             label="Phone"
             placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={ text => this.props.employeeUpdate({ prop: 'phone', value: text })}
           />
         </CardSection>
 
@@ -30,4 +35,10 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm
+
+  return { name, phone, shift }
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate)
