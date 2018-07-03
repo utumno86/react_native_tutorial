b3df7fe7c6be64a77d36d5e556af256bf7f4aa8b
Michael Nash <michael@spatialnetworks.com>
2018-07-03 16:21:23 -0400

    wrote modal component

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -3,10 +3,12 @@ import Communications from 'react-native-communications'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import { employeeUpdate, employeeSave } from '../actions'
-import { Card, CardSection, Button } from './common'
+import { Card, CardSection, Button, Confirm } from './common'
 import EmployeeForm from './EmployeeForm'
 
 class EmployeeEdit extends Component {
+  state = { showModal: false }
+
   componentWillMount () {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value })
@@ -39,6 +41,14 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+        <Confirm visible={this.state.showModal}>
+          Are you sure you want to fire this person?
+        </Confirm>
       </Card>
     )
   }
